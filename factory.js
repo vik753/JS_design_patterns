e6b43e47d73a6a33c8c5924f5bb49ddb5763eba6
Igor Korenets <vik753@gmail.com>
2019-12-15 11:20:35 +0200

    create design pattern Factory

---
+++ factory.js
@@ -0,0 +1,51 @@
+class SimpleMembership {
+    constructor(name) {
+        this.name = name;
+        this.cost = 50;
+    }
+}
+
+class StandardMembership {
+    constructor(name) {
+        this.name = name;
+        this.cost = 100;
+    }
+}
+
+class PremiunMembership {
+    constructor(name) {
+       this.name = name;
+       this.cost = 150;
+    }
+}
+
+class MemberFactory {
+    static list = {
+        simple: SimpleMembership,
+        standard: StandardMembership,
+        premium: PremiunMembership
+    };
+
+    create(name = 'Anonymous', type = 'simple') {
+        const Membership = MemberFactory.list[type] || MemberFactory.list.simple;
+        const member = new Membership(name);
+        member.type = type;
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        };
+        return member;
+    }
+}
+
+const factory = new MemberFactory();
+const members = [
+    factory.create(),
+    factory.create('Andrew', 'simple'),
+    factory.create('Andrew2', 'standard'),
+    factory.create('Andrew3', 'premium'),
+    factory.create('Andrew4'),
+];
+
+members.forEach(member => {
+member.define()
+});
